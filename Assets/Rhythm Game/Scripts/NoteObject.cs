361424f2439fe7d45933400f2b5bf9b8875a49e6
troy <leeiam2.magsipoc@live.uwe.ac.uk>
2026-03-12 01:48:53 +0000

    Basics
    - Added NoteObject Script
    - Added Block and Note Hitbox
    - Notes disappear when hit correctly

---
+++ Assets/Rhythm Game/Scripts/NoteObject.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canbePressed;
+
+    public KeyCode keyToPress;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            if (canbePressed)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canbePressed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canbePressed = false;
+        }
+    }   
+}
